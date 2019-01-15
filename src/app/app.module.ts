638129a77ed124a29966e20eebbfe9a4ab7f01de
Alfonso Esteve <alfonsem@gmail.com>
2019-01-15 15:33:51 +0100

    añadido componente home y movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesService } from './movies.service';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule,HttpClientModule
+    BrowserModule,HttpClientModule, AppRoutingModule
   ],
   providers: [MoviesService],
   bootstrap: [AppComponent]
