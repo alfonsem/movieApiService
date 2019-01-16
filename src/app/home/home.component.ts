59da052d22580383e4307f03d32250c0ab284b0c
Alfonso Esteve <alfonsem@gmail.com>
2019-01-16 09:27:36 +0100

    añadidos los comentarios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,40 @@
 import { Component, } from '@angular/core';
-import { MoviesService } from '../movies.service';
+import { MoviesService } from '../movies.service'; //Importamos el servicio desde donde cargaremos las "movies"
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   imgDomain: string;
   data: Array<object>;
   title: string = '';
+
+  //Constructor de la clase donde cargamos el dominio de la imagen de cada "movie"
   constructor(private api: MoviesService) {
     this.imgDomain = api.getImgDomain();
-    console.log (this.imgDomain);
+    // console.log (this.imgDomain);
   }
+
+  //Para cargar las "popular movies" a través del servicio "api."
   loadPopularMovies() {
     this.api.popularMovies().then((result: any) => {
       this.data = result.results;
       this.title = 'Popular movies';
     });
   }
+
+  //Cargar las "trending movies" a través del servicio con "api."
   loadTrendingMovies() {
     this.api.trendingMovies().then((result: any) => {
       this.data = result.results;
       this.title = 'Trending movies';
     });
   }
+
+  //Para limpiar el array de datos
   clean() {
     this.data = undefined;
   }
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MoviesService } from '../movies.service';
-import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../movies.service'; //Importamos el servicio para cargar los datos de la "movie"
+import { ActivatedRoute } from '@angular/router'; //Importamos para routear el link
 
 @Component({
   selector: 'app-movie-detail',
@@ -12,10 +12,12 @@ export class MovieDetailComponent implements OnInit {
   movieData: object;
   imgDomain: string;
 
+  //Constructor de la clase donde cargamos el dominio de la imagen de la "movie"
   constructor(private movieService: MoviesService, private route: ActivatedRoute) {
     this.imgDomain = movieService.getImgDomain();
   }
 
+  //Al inicio recogemos el id de "movie"
   ngOnInit() {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.movieService.getMovie(this.id).then(data => {
